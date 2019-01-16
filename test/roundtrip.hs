f3f23fde4dcd7dbdbe17dc731100e94884a3d936
John MacFarlane <jgm@berkeley.edu>
2019-01-16 11:49:33 -0800

    Added rountrip test.

---
+++ test/roundtrip.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Ipynb
+import Data.Aeson (Value(..), eitherDecode, encode)
+import Data.Aeson.Diff
+import System.Environment
+import System.FilePath
+import qualified Data.ByteString.Lazy as BL
+import Test.Tasty
+import Test.Tasty.HUnit
+import System.Directory
+import Data.Text.IO as T
+import Lens.Micro
+import Lens.Micro.Aeson
+import qualified Data.Text as T
+import qualified Data.HashMap.Strict as HM
+import qualified Data.ByteString.Base64 as Base64
+
+main :: IO ()
+main = do
+  let rtdir = "test" </> "rt-files"
+  createDirectoryIfMissing False rtdir
+  fs <- map (rtdir </>) . filter isIpynb <$> getDirectoryContents rtdir
+  defaultMain $ testGroup "round-trip tests" $ map rtTest fs
+
+isIpynb :: FilePath -> Bool
+isIpynb fp = takeExtension fp == ".ipynb"
+
+-- We don't want tests failing because of inconsequential
+-- differences in formatting of base64 data, like line breaks.
+normalizeBase64 :: BL.ByteString -> BL.ByteString
+normalizeBase64 bs =
+  bs & key "cells" . values . key "outputs" . values . key "data"
+       . _Object %~ HM.mapWithKey (\k v ->
+                       if k == "application/json" ||
+                          "text/" `T.isPrefixOf` k
+                          then v
+                          else case v of
+                            String t -> String $ (T.replace "\n" "" t) <> "\n"
+                            _ -> v)
+rtTest :: FilePath -> TestTree
+rtTest fp = testCase fp $ do
+  inRaw <- BL.readFile fp
+  (inJSON :: Value) <- either error return $ eitherDecode inRaw
+  (nb :: Notebook NbV4) <- either error return $ eitherDecode
+                             $ normalizeBase64 inRaw
+  let outRaw = encode nb
+  (outJSON :: Value) <- either error return $ eitherDecode
+                             $ normalizeBase64 outRaw
+  -- test that (read . write) == id
+  let patch = diff inJSON outJSON
+  assertBool (show patch) (patch == Patch [])
+  -- now test that (write . read) == id
+  (nb' :: Notebook NbV4) <- either error return $ eitherDecode outRaw
+  assertEqual "write . read != read" nb nb'
