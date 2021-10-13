cf56acc3f2dce373fa25e1ca484c4d01391499e3
John MacFarlane <jgm@berkeley.edu>
2021-10-12 19:17:44 -0700

    Changes for aeson 2 compatibility.
    Simplified round trip test to avoid use of aeson-diff, which
    still doesn't work with aeson 2.

--- src/Data/Ipynb.hs
+++ src/Data/Ipynb.hs
@@ -48,7 +48,6 @@ import Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Base64 as Base64
 import Data.Char (isSpace)
-import qualified Data.HashMap.Strict as HM
 import Data.List (partition)
 import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
@@ -61,7 +60,13 @@ import Prelude
 #else
 import Data.Semigroup
 #endif
-
+#if MIN_VERSION_aeson(2,0,0)
+import qualified Data.Aeson.KeyMap as KM
+#else
+import qualified Data.HashMap.Strict as KM
+#endif
+import Data.String
+import qualified Data.Set as Set
 
 -- | Indexes 'Notebook' for serialization as nbformat version 3.
 data NbV3
@@ -269,18 +274,17 @@ metadataToV3Pairs :: JSONMeta -> [Aeson.Pair]
 metadataToV3Pairs meta =
   ("metadata" .= M.fromList regMeta) : map toPair extraMeta
   where (extraMeta, regMeta) = partition isExtraMeta $ M.toList meta
-        toPair (k,v) = k .= v
+        toPair (k,v) = (fromString (T.unpack k)) .= v
+        isExtraMeta (k,_) = k `Set.member` v3MetaInMainCell
 
-v3MetaInMainCell :: [Text]
-v3MetaInMainCell = ["collapsed", "language"]
+v3MetaInMainCell :: Set.Set Text
+v3MetaInMainCell = Set.fromList ["collapsed", "language"]
 
-isExtraMeta :: (Text, a) -> Bool
-isExtraMeta (k,_) = k `elem` v3MetaInMainCell
-
-parseV3Metadata :: HM.HashMap Text Value -> Aeson.Parser JSONMeta
+parseV3Metadata :: Aeson.Object -> Aeson.Parser JSONMeta
 parseV3Metadata v = do
   meta <- v .:? "metadata" .!= mempty
-  let extraMeta = M.fromList $ filter isExtraMeta $ HM.toList v
+  vm <- parseJSON (Object v)
+  let extraMeta = M.restrictKeys vm v3MetaInMainCell
   return (meta <> extraMeta)
 
 -- | Information about the type of a notebook cell, plus
@@ -382,7 +386,7 @@ extractNbV3Data v = do
       go ("jpeg", x)          = Just ("image/jpeg", x)
       go ("javascript", x)    = Just ("application/javascript", x)
       go (_, _)               = Nothing -- TODO complete list? where documented?
-  parseJSON (Object . HM.fromList . mapMaybe go . HM.toList $ v)
+  parseJSON (Object . KM.fromList . mapMaybe go . KM.toList $ v)
 
 instance ToJSON (Output NbV4) where
   toJSON s@Stream{} = object
@@ -434,11 +438,11 @@ instance ToJSON (Output NbV3) where
 
 adjustV3DataFields :: Value -> Value
 adjustV3DataFields (Object hm) =
-  case HM.lookup "data" hm of
+  case KM.lookup "data" hm of
     Just (Object dm) -> Object $
-      HM.delete "data" $ foldr
-      (\(k, v) -> HM.insert (modKey k) v) hm
-      (HM.toList dm)
+      KM.delete "data" $ foldr
+      (\(k, v) -> KM.insert (modKey k) v) hm
+      (KM.toList dm)
     _ -> Object hm
   where  modKey "text/plain"             = "text"
          modKey "text/latex"             = "latex"
--- test/roundtrip.hs
+++ test/roundtrip.hs
@@ -1,16 +1,15 @@
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Control.Monad (unless)
 import Data.Aeson (Value (..), eitherDecode, encode)
-import Data.Aeson.Diff as AesonDiff
-import qualified Data.ByteString.Base64 as Base64
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString as B
-import qualified Data.HashMap.Strict as HM
 import Data.Ipynb
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Encoding as TE
-import qualified Data.Vector as V
+import qualified Data.Text.Lazy.Encoding as TLE
 import Lens.Micro
 import Lens.Micro.Aeson
 import System.Directory
@@ -29,73 +28,27 @@ main = do
 isIpynb :: FilePath -> Bool
 isIpynb fp = takeExtension fp == ".ipynb"
 
--- We don't want tests failing because of inconsequential
--- differences in formatting of base64 data, like line breaks.
-normalizeBase64 :: Value -> Value
-normalizeBase64 bs =
-  bs & key "cells" . values . key "outputs" . values . key "data"
-       . _Object %~ HM.mapWithKey (\k v ->
-                       if k == "application/json" ||
-                          "text/" `T.isPrefixOf` k ||
-                          "+json" `T.isSuffixOf` k
-                          then v
-                          else go v)
-  where
-     go (Array vec) =
-       go $ String
-          $ mconcat $ map
-              (\v' -> case v' of
-                        String t' -> t'
-                        _         -> error "expected String") $ V.toList vec
-     go (String t) =
-       case Base64.decode (TE.encodeUtf8 (T.filter (/='\n') t)) of
-            Left _  -> String t  -- textual
-            Right b -> String $
-              TE.decodeUtf8 .  (<> "\n") .
-              B.intercalate "\n" .  chunksOf 76 .
-              Base64.encode $ b
-     go v = v
-
-chunksOf :: Int -> B.ByteString -> [B.ByteString]
-chunksOf k s
-   | B.null s = []
-   | otherwise =
-     let (h,t) = B.splitAt k s
-     in h : chunksOf k t
-
 rtTest :: FilePath -> TestTree
 rtTest fp = testCase fp $ do
   inRaw <- BL.readFile fp
-  let format = inRaw ^? key "nbformat"._Number
+  let format = inRaw ^? key "nbformat" . _Number
   case format of
     Just 4 -> rtTest4 inRaw
     _      -> rtTest3 inRaw
 
 rtTest3 :: BL.ByteString -> IO ()
 rtTest3 inRaw = do
-  (inJSON :: Value) <- either error return $ eitherDecode inRaw
   (nb :: Notebook NbV3) <- either error return $ eitherDecode inRaw
   let outRaw = encode nb
   (nb' :: Notebook NbV3) <- either error return $ eitherDecode outRaw
-  (outJSON :: Value) <- either error return $ eitherDecode outRaw
-  -- test that (read . write) == id
-  let patch' = AesonDiff.diff
-         (normalizeBase64 inJSON) (normalizeBase64 outJSON)
-  assertBool (show patch') (patch' == Patch [])
-  -- now test that (write . read) == id
+  -- test that (write . read) == id
   assertEqual "write . read != read" nb nb'
 
 rtTest4 :: BL.ByteString -> IO ()
 rtTest4 inRaw = do
-  (inJSON :: Value) <- either error return $ eitherDecode inRaw
   (nb :: Notebook NbV4) <- either error return $ eitherDecode inRaw
   let outRaw = encode nb
   (nb' :: Notebook NbV4) <- either error return $ eitherDecode outRaw
-  (outJSON :: Value) <- either error return $ eitherDecode outRaw
-  -- test that (read . write) == id
-  let patch' = AesonDiff.diff
-       (normalizeBase64 inJSON) (normalizeBase64 outJSON)
-  assertBool (show patch') (patch' == Patch [])
-  -- now test that (write . read) == id
+  -- test that (write . read) == id
   assertEqual "write . read != read" nb nb'
 
